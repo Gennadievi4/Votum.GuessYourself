87053424673017d50ed4b12abafc058f464de5cf
Gennadiy <gonzy@yandex.ru>
2020-09-07 17:23:13 +0400

    Перенёс разметку главного окна

---
+++ GuessYouSelf.Core/Converters/ActualHeightTriggerToVisibility.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Globalization;
+using System.Windows;
+using System.Windows.Data;
+
+namespace GuessYourself.Core.Converters
+{
+    class ActualHeightTriggerToVisibility : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return value is double size && size <= 1000 ? Visibility.Collapsed : Visibility.Visible;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return value;
+        }
+    }
+}
---
+++ GuessYouSelf.Core/Converters/ActualHeightTriggerToVisibilityForButton.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Globalization;
+using System.Windows;
+using System.Windows.Data;
+
+namespace GuessYourself.Core.Converters
+{
+    class ActualHeightTriggerToVisibilityForButton : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return value is double size && size <= 1000 ? Visibility.Visible : Visibility.Collapsed;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
